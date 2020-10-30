c9fb6fbcfdd527580af2c79bac626c652d337167
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-10-30 10:39:22 +0100

    fixed the error in the arduino code

--- MainProgram/MainProgram.ino
+++ MainProgram/MainProgram.ino
@@ -2,9 +2,9 @@ int analogPin = A0;
 int groundPin = 13;
 
 int delayValue = 200;
-int thresholdLevel = 20;
+int thresholdLevel = 25;
 int value = 0;
-int del = 0;
+unsigned long del = 0;
 
 void setup() {
   analogReference(INTERNAL);
@@ -17,9 +17,9 @@ void loop() {
 
   value = analogRead(analogPin);
   Serial.println(value);
-  if(value <= thresholdLevel) //state changes on input
+  if (value <= thresholdLevel) //state changes on input
   {
-    if(del + delayValue < millis())
+    if (del + delayValue < millis())
     {
       digitalWrite(groundPin, LOW);
     }
@@ -29,25 +29,28 @@ void loop() {
     del = millis();
     digitalWrite(groundPin, HIGH);
   }
-  if(Serial.available()) //configuration part. 
+  if (Serial.available()) //configuration part.
   {
     String text = Serial.readString();
-      analogWrite(LED_BUILTIN,200);
-    if(text[0] == 'A')
+    analogWrite(LED_BUILTIN, 200);
+    if (text[0] == 'A')
     {
       delay(2000);
-      Serial.write("I AM HERE",9);
+      Serial.write("I AM HERE", 9);
     }
     else
     {
-      char value[(text.length())-1];
-      for(int i = 1; i < text.length(); i++){
-        value[i-1] = text[i];
+      char value[(text.length()) - 1];
+      for (int i = 1; i < text.length(); i++) 
+      {
+        value[i - 1] = text[i];
       }
-      if(text[0] == 'T'){
+      if (text[0] == 'T') 
+      {
         thresholdLevel = atoi(value);
       }
-      if(text[0] == 'D'){
+      if (text[0] == 'D') 
+      {
         delayValue = atoi(value);
       }
       delay(1000);
